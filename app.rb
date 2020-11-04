9e88da151ec0d1be62822245d09f3331b605c226
Matt Wright <mttwright@gmail.com>
2020-11-04 13:39:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   # Add your post route and action below
   
   post '/food' do
-    "Bologna"
+    "Greetings #{params[:name]}, who's favorite food is #{params[:favorite_food]}"
   end
 
 end
